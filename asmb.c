709a4894294bc7e012df33425346777b6b80a628
Joe Allen <jhallenworld@gmail.com>
2026-02-12 18:07:05 -0500

    Assembler

--- asmb.c
+++ asmb.c
@@ -1,6 +1,6 @@
 /*	EXORcister simulator
  *	Copyright
- *		(C) 2011 Joseph H. Allen
+ *		(C) 2026 Joseph H. Allen
  *
  * This is free software; you can redistribute it and/or modify it under the 
  * terms of the GNU General Public License as published by the Free Software 
